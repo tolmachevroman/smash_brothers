a489315c675ec2a019445e8e006dff62d6fcb32e
tolmachevroman <tolmachevroman@gmail.com>
2021-10-13 18:58:25 -0300

    add Hilt SharedPreferences module

--- app/src/main/java/com/koombea/smash/bros/viewmodels/FiltersViewModel.kt
+++ app/src/main/java/com/koombea/smash/bros/viewmodels/FiltersViewModel.kt
@@ -1,10 +1,19 @@
 package com.koombea.smash.bros.viewmodels
 
+import android.content.SharedPreferences
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
-class FiltersViewModel @Inject constructor() : ViewModel() {
+class FiltersViewModel @Inject constructor(private val sharedPreferences: SharedPreferences) :
+    ViewModel() {
 
+        fun getFilterSettings() {
+//            val s = sharedPreferences.getString()
+        }
+
+        fun saveFilterSettings() {
+//            sharedPreferences.edit().
+        }
 }
