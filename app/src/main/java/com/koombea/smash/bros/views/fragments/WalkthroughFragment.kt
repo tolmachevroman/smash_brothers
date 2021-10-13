4dab09508298a3362884c9a90464adb5af237582
tolmachevroman <tolmachevroman@gmail.com>
2021-10-13 12:34:22 -0300

    add Walkthrough Fragment

--- app/src/main/java/com/koombea/smash/bros/views/fragments/WalkthroughFragment.kt
+++ app/src/main/java/com/koombea/smash/bros/views/fragments/WalkthroughFragment.kt
@@ -6,32 +6,23 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
-import com.koombea.smash.bros.databinding.LauncherFragmentBinding
+import com.koombea.smash.bros.databinding.WalkthroughFragmentBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class LauncherFragment : Fragment() {
+class WalkthroughFragment : Fragment() {
 
-    private lateinit var binding: LauncherFragmentBinding
+    private lateinit var binding: WalkthroughFragmentBinding
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding = LauncherFragmentBinding.inflate(layoutInflater)
+        binding = WalkthroughFragmentBinding.inflate(inflater)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        // TODO check if launched for the 1st time, or skip
-
-        //navigate to Walkthrough after 2 seconds
-        view.postDelayed({
-            val directions = LauncherFragmentDirections.navigateToWalkthrough()
-            findNavController().navigate(directions)
-        }, 2000)
     }
-
 }
